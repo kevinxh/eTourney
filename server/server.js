15e85194d93b7496c89c77b03d21b417779beea4
Kevin <kevintxhe933@hotmail.com>
2016-04-28 15:55:35 -0700

    fixed: passport() is not a function

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 var express = require('./config/express'),
 	mongoose = require('./config/mongoose'),
-	passport = require('./config/passport');
+	Passport = require('./config/passport');
 
 module.exports = function() {
 
 	var db = mongoose();
 	var app = express();
-	var	passport = passport();
+	var	passport = Passport();
 
 	return app;
 };
