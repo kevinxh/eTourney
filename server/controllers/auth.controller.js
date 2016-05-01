cd31c61454f422c2ec23e1c7cc1b6e6e738e6296
Kevin <kevintxhe933@hotmail.com>
2016-05-01 13:40:55 -0700

    check post method parameter for login

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -4,41 +4,48 @@ var User = require('mongoose').model('User'),
 
 exports.login = function(req, res) {
 
-	User.findOne({
-		email: req.body.email
-  	}, function(err, user) {
-    	if (err) 	return 	res.json({
-    							success: false, 
-    							msg: err
-    						});
+	if (req.body.email && req.body.password)
+		User.findOne({
+			email: req.body.email
+	  	}, function(err, user) {
+	    	if (err) 	return 	res.json({
+	    							success: false, 
+	    							msg: err
+	    						});
 
-    	if (!user) {
-      		return 	res.json({
-      					success: false,
-      					msg: 'Authentication failed. User not found.'
-      				});
-    	} else {
-      	// Check if password matches
-      		user.authenticate(req.body.password, function(err, isMatch) {
-        		if (isMatch) {
-          			// Create token if the password matched and no error was thrown
-          			var token = jwt.sign({email:user.email}, config.JwtSecret, {
-            						expiresIn: 10080 // a week in seconds
-          						});
-		          	return 	res.json({
-		          				success: true,
-		          				email: user.email,
-		          				access_token: 'JWT '+token 
-		          			});
-		        } else {
-		          	return 	res.json({
-		          				success: false,
-		          				msg: 'Authentication failed. Passwords did not match.'
-		          			});
-		        }
-      		});
-    	}
-  });
+	    	if (!user) {
+	      		return 	res.json({
+	      					success: false,
+	      					msg: 'Authentication failed. User not found.'
+	      				});
+	    	} else {
+	      	// Check if password matches
+	      		user.authenticate(req.body.password, function(err, isMatch) {
+	        		if (isMatch) {
+	          			// Create token if the password matched and no error was thrown
+	          			var token = jwt.sign({email:user.email}, config.JwtSecret, {
+	            						expiresIn: 10080 // a week in seconds
+	          						});
+			          	return 	res.json({
+			          				success: true,
+			          				email: user.email,
+			          				access_token: 'JWT '+token 
+			          			});
+			        } else {
+			          	return 	res.json({
+			          				success: false,
+			          				msg: 'Authentication failed. Passwords did not match.'
+			          			});
+			        }
+	      		});
+	    	}
+	  });
+	} else {
+		return 	res.json({
+			    	success: false,
+			        msg: 'Please enter your email and password.'
+			    });
+	}
   
 };
 
