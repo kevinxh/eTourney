8c3465e9cc89253bc8fd11a1ad73a68633ed44f1
aaron <aaron931122@gmail.com>
2016-05-13 14:12:41 -0700

    tab selection behaviour done

--- client/components/createTM/createTM-tabs.js
+++ client/components/createTM/createTM-tabs.js
@@ -8,8 +8,14 @@ export default class CreateTMtabs extends Component {
     super(props);
     this.state = {
       selectedTab: 1,
+      tabState: {
+        tab1: true,
+        tab2: false,
+        tab3: false,
+      },
     };
     this.Activekey = this.Activekey.bind(this);
+    this.onSelect = this.onSelect.bind(this);
   }
 
   Activekey(key) {
@@ -17,18 +23,39 @@ export default class CreateTMtabs extends Component {
     this.setState({selectedTab:key});
   }
 
+  handleTabState(tab,active) {
+
+    this.setState({
+      selectedTab:active,
+      tabState: {
+        ...this.state.tabState,
+        [tab]: true,
+      },
+    }
+    );
+  }
+
+  onSelect(event){
+    console.log(event);
+    if(this.state.tabState[`tab${event}`]){
+    this.setState({selectedTab:event});
+  }
+    // todo : error message feedback
+  }
+
   render() {
     return (<div>
-      <Tabs defaultActiveKey={1}
+      <Tabs
+        defaultActiveKey={1}
         activeKey={this.state.selectedTab}
-        id="uncontrolled-tab-example">
+        onSelect={this.onSelect}
+        id="createTM"
+      >
         <Tab eventKey={1} title="Tab 1">Tab 1 content</Tab>
-        <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
-        <Tab eventKey={3} title="Tab 3">Tab 3 content</Tab>
+        <Tab eventKey={2} title="Tab 2" disable={this.state.tabState.tab2}>Tab 2 content</Tab>
+        <Tab eventKey={3} title="Tab 3" disable={this.state.tabState.tab3}>Tab 3 content</Tab>
       </Tabs>
-      <button onClick={()=> this.Activekey(1)}>1</button>
-      <button onClick={()=> this.Activekey(2)}>2</button>
-      <button onClick={()=> this.Activekey(3)}>3</button>
+      <button onClick={()=> this.handleTabState(`tab${this.state.selectedTab+1}`,this.state.selectedTab+1)}>next</button>
       </div>);
   }
 
