2d257f31cb73f2a8d98bf8ec6bd03c3c08094b6c
Kevin <kevintxhe933@hotmail.com>
2016-05-18 20:52:09 -0700

    tournament basic db schema done

--- server/controllers/api/tournament.controller.js
+++ server/controllers/api/tournament.controller.js
@@ -1,3 +1,27 @@
+import Tournament from '../../models/tournament';
+
 export function create(req, res) {
-  return res.json({ testing: '123' });
+  if (!req.body.tournamentName) {
+    return res.status(400).json({
+      success: false,
+      msg: 'Please enter your tournament information',
+    });
+  }
+  const tournament = new Tournament({
+    tournamentName: req.body.tournamentName,
+    game: req.body.game,
+    creatorEmail: req.user.email,
+  });
+  tournament.save((err) => {
+    if (err) {
+      return	res.status(401).json({
+        success: false,
+        msg: err,
+      });
+    }
+    return res.status(201).json({
+      success: true,
+      tournamentName: tournament.tournamentName,
+    });
+  });
 }
