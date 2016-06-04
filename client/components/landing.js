3cf8c480195edd728176f8d9a4461398a032705f
Kevin <kevintxhe933@hotmail.com>
2016-06-04 02:38:01 -0700

    landing section

---
+++ client/components/landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+require('../style/_landing.scss');
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <section className="full-vh">
+        <div className="bg-wrapper">
+
+        </div>
+      </section>
+		);
+  }
+}
