19538933a5748cee2422a173fa26042b9b313645
Kevin <kevintxhe933@hotmail.com>
2016-06-27 12:59:40 -0700

    tournament-list-item progress bar

--- client/components/tournament/tournament-list-item.js
+++ client/components/tournament/tournament-list-item.js
@@ -2,7 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { push } from 'react-router-redux';
-import { Image } from 'react-bootstrap';
+import { Link } from 'react-router';
+import ProgressBar from 'react-bootstrap/lib/ProgressBar';
 
 class TournamentListItem extends Component {
   constructor(props) {
@@ -14,9 +15,9 @@ class TournamentListItem extends Component {
     this.props.push(`/${this.props.tournament.tournamentName}`);
   }
 
-  image() {
+  image(url) {
     const img = {
-      backgroundImage: 'url(' + 'http://placehold.it/300x150' + ')',
+      backgroundImage: `url(${url})`,
     };
     return img;
   }
@@ -32,14 +33,16 @@ class TournamentListItem extends Component {
     } = this.props.tournament;
     return (
       <div className="tournament-list-item" onClick={this.onClick}>
-        <div className="tournament-image" style={this.image()} />
+        <div className="tournament-image" style={this.image('http://placehold.it/300x150')} />
         <div className="content-area">
           <div className="content-title">
             <div className="title">{tournamentName}</div>
-            <div className="creator">{creatorEmail}</div>
+            <div className="creator">
+              <Link to="/">{creatorEmail}</Link>
+            </div>
           </div>
           <hr />
-          <table className="table">
+          <table className="content-detail table">
             <tbody>
               <tr>
                 <td>游戏</td>
@@ -53,8 +56,10 @@ class TournamentListItem extends Component {
                 <td>比赛类型</td>
                 <td>{type}</td>
               </tr>
+
             </tbody>
           </table>
+          <ProgressBar now={60} label={'60/100'} />
         </div>
       </div>
     );
