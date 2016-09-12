7f7ac9eb6e914b24a88507cba3e704d4f8ea41ea
Kevin <kevintxhe933@hotmail.com>
2016-09-12 16:27:30 -0700

    added datepicker

--- client/components/createTM/tab-setting-LOL.js
+++ client/components/createTM/tab-setting-LOL.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react'
 import Input from '../form-elements/input'
-//import { DatePicker } from 'react-toolbox/lib/date_picker'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
 
 export default class TabSettingLOL extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      startDate: moment(),
+      endDate: moment(),
+    }
+    this.handleStartDateChange = this.handleStartDateChange.bind(this)
+    this.handleEndDateChange = this.handleEndDateChange.bind(this)
+  }
+
+  handleStartDateChange(startDate) {
+    this.setState({
+      startDate,
+    })
+  }
+
+  handleEndDateChange(endDate) {
+    this.setState({
+      endDate,
+    })
+  }
+
   render() {
     return (
       <div>
         <Input
           id="比赛名"
           placeholder="请输入3-20位比赛名称"
+        />
+        <DatePicker
+          todayButton={"今天"}
+          selected={this.state.startDate}
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleStartDateChange}
+        />
+        <DatePicker
+          todayButton={"今天"}
+          selected={this.state.endDate}
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleEndDateChange}
         />
       </div>)
   }
