8f2b52e799d67763176bc493de5241e8329fe131
Jack Chao <euleriepsilon@gmail.com>
2016-09-19 15:31:56 -0700

    Fixed linter errors in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require('webpack')
+const path = require('path')
 // const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './client/index.js'
+    './client/index.js',
   ],
   watch: true,
   output: {
     path: path.join(__dirname, './client/assets'),
     filename: 'bundle.js',
-    publicPath: 'http://0.0.0.0:8080/static/'
+    publicPath: 'http://0.0.0.0:8080/static/',
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
@@ -26,22 +26,27 @@ module.exports = {
         test: /\.js$/,
         loaders: ['babel'],
         exclude: /node_modules/,
-        include: __dirname
+        include: __dirname,
       }, {
         test: /\.(scss|sass)$/,
         loaders: ['style-loader',
           'css-loader?sourceMap',
           'postcss-loader',
-          'sass-loader?sourceMap&includePaths[]=' + path.resolve(__dirname, "./node_modules/compass-mixins/lib")]
+          'sass-loader?sourceMap&includePaths[]=' + path.resolve(__dirname, './node_modules/compass-mixins/lib')],
       }, {
         test: /\.(jpe?g|png|gif|svg)$/,
-        loader: 'url-loader?limit=10000&name=[path][name].[ext]'
-      }
-    ]
-      //loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!postcss-loader!sass-loader?sourceMap') }]
+        loader: 'url-loader?limit=10000&name=[path][name].[ext]',
+      },
+    ],
+      // loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!postcss-loader!sass-loader?sourceMap') }]
   },
   postcss: [autoprefixer({ remove: false, browsers: ['last 3 versions'] })],
   resolve: {
-    extensions: ['', '.js', '.jsx']
-  }
-};
+    extensions: ['', '.js', '.jsx'],
+  },
+  externals: {
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
+}
